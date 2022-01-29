421bd8fc540e76e08a7a3f91907db6cac6c471ae
Martin N <na@na.no>
2022-01-30 00:29:54 +0100

    update libs and runtime

--- RLKeyDiscordBot/Program.cs
+++ RLKeyDiscordBot/Program.cs
@@ -8,63 +8,62 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
-namespace RLKeyDiscordBot
+namespace RLKeyDiscordBot;
+
+public class Program
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-			=> new Program().MainAsync().GetAwaiter().GetResult();
+    public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
 
-        private DiscordSocketClient _client;
-        private IConfigurationRoot _config;
+    private DiscordSocketClient _client;
+    private IConfigurationRoot _config;
 
-        public async Task MainAsync()
-        {
+    public async Task MainAsync()
+    {
 
-            //Create the configuration
-            const string environment = "development";
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("config.json")
-                .AddJsonFile($"config.{environment}.json", optional: true);
-            _config = builder.Build();
-            // You should dispose a service provider created using ASP.NET
-            // when you are finished using it, at the end of your app's lifetime.
-            // If you use another dependency injection framework, you should inspect
-            // its documentation for the best way to do this.
-            using var services = ConfigureServices();
-            var client = services.GetRequiredService<DiscordSocketClient>();
+        //Create the configuration
+        const string environment = "development";
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("config.json")
+            .AddJsonFile($"config.{environment}.json", optional: true);
+        _config = builder.Build();
+        // You should dispose a service provider created using ASP.NET
+        // when you are finished using it, at the end of your app's lifetime.
+        // If you use another dependency injection framework, you should inspect
+        // its documentation for the best way to do this.
+        var services = ConfigureServices();
+        var client = services.GetRequiredService<DiscordSocketClient>();
 
-            client.Log += LogAsync;
-            services.GetRequiredService<CommandService>().Log += LogAsync;
+        client.Log += LogAsync;
+        services.GetRequiredService<CommandService>().Log += LogAsync;
 
 
-            // Tokens should be considered secret data and never hard-coded.
-            // We can read from the environment variable to avoid hardcoding.
-            await client.LoginAsync(TokenType.Bot, _config["DiscordBotToken"]);
-            await client.StartAsync();
+        // Tokens should be considered secret data and never hard-coded.
+        // We can read from the environment variable to avoid hardcoding.
+        await client.LoginAsync(TokenType.Bot, _config["DiscordBotToken"]);
+        await client.StartAsync();
 
-            // Here we initialize the logic required to register our commands.
-            await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
-            await Task.Delay(-1);
-        }
+        // Here we initialize the logic required to register our commands.
+        await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+        await Task.Delay(-1);
+    }
 
-        private Task LogAsync(LogMessage log)
-        {
-            Console.WriteLine(log.ToString());
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log.ToString());
 
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
+    }
 
-        private ServiceProvider ConfigureServices()
-        {
-            return new ServiceCollection()
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton(_config)
-                .AddSingleton<CommandService>()
-                .AddSingleton<CommandHandlingService>()
-                .AddSingleton<HttpClient>()
-                .AddSingleton<Fileservice>()
-                .BuildServiceProvider();
-        }
+    private IServiceProvider ConfigureServices()
+    {
+        return new ServiceCollection()
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton(_config)
+            .AddSingleton<CommandService>()
+            .AddSingleton<CommandHandlingService>()
+            .AddSingleton<HttpClient>()
+            .AddSingleton<Fileservice>()
+            .BuildServiceProvider();
     }
 }
